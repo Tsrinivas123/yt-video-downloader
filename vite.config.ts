8ad360b7f672bd0f23a331d68bda759a253a42c1
Tsrinivas <tushashrivas7999@gmail.com>
2026-02-07 16:14:58 +0530

    Fix vite base path for render

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: './', // ✅ IMPORTANT FOR RENDER
+    base: '/', // ✅ IMPORTANT FOR RENDER
 
     server: {
       port: 3000,
